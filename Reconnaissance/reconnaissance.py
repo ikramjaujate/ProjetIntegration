dff1d2650c9bf159fce72b91ec737611c003daf5
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-09-25 18:54:13 +0200

    Fichier reconnaissance facial via photo : à modifier

--- Reconnaissance/reconnaissance.py
+++ Reconnaissance/reconnaissance.py
@@ -1,35 +1,45 @@
+import numpy as np
+import face_recognition as fr
 import cv2
-import sys
-
-cascPath = sys.argv[0]
-faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
+ikram_image = fr.load_image_file("/Users/ikramjaujate/Desktop/ProjetIntegration-1/Reconnaissance/ikram.JPG")
+ikram_face_encoding = fr.face_encodings(ikram_image)[0]
+
+known_face_encondings = [ikram_face_encoding]
+known_face_names = ["Ikram"]
+
+while True: 
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb_frame = frame[:, :, ::-1]
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+    face_locations = fr.face_locations(rgb_frame)
+    face_encodings = fr.face_encodings(rgb_frame, face_locations)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        matches = fr.compare_faces(known_face_encondings, face_encoding)
+
+        name = "Unknown"
+
+        face_distances = fr.face_distance(known_face_encondings, face_encoding)
+
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+        
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Webcam_facerecognition', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
