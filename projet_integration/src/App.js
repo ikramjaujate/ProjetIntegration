d2afd12deb3d04ff96d5cd338e7b350ad85d3e7b
Clémentine <sacre.clementine@gmail.com>
2021-09-27 18:16:57 +0200

    Mise en place bootstrap

--- projet_integration/src/App.js
+++ projet_integration/src/App.js
@@ -1,11 +1,26 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './css/App.css';
 import Navbar from './components/Navbar.js';
+import Grades from './Grades';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar/>
+        <div className="content">
+          <Switch>
+
+            <Route exact path="/grades"> 
+              <Grades/>
+            </Route>
+
+          </Switch>
+
+        </div>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ projet_integration/src/Grades.js
@@ -0,0 +1,14 @@
+import'bootstrap/dist/css/bootstrap.min.css';
+import'bootstrap/dist/js/bootstrap.bundle.min';
+
+function Grades() {
+    return (
+        <div>
+            <div classname="row">
+                <div classname="col-sm-4"></div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Grades;
