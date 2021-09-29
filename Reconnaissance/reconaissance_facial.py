776fa95377502f40a71386189a5fd55c9f26675a
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-09-29 18:25:56 +0200

    Enregistrement visag ependant reconnaissance

--- Reconnaissance/reconaissance_facial.py
+++ Reconnaissance/reconaissance_facial.py
@@ -30,15 +30,20 @@ print('Encodings Complete')
 
 # capture vidéo
 cap = cv2.VideoCapture(0)
+
+#writer = cv2.VideoWriter("output.avi",cv2.VideoWriter_fourcc(*"MJPG"), 30,(640,480))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('video.avi', fourcc, 20, (640, 480))
 while True:
     success, img = cap.read()
+    #writer.write(img)
     imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) # redimensionner pour garder les performances
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    
+    out.write(img)
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
@@ -64,5 +69,8 @@ while True:
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
         
     cv2.imshow('Webcam',img)
+    cv2.imwrite("pic.png", img)
     cv2.waitKey(1)
+
+    
 
