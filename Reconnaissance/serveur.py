917df5a555b473697006e7fa3084eba944202bdd
Corentin Dallenogare <he201885@students.ephec.be>
2021-10-04 15:02:58 +0200

    ajout camera sur ip sans flask

--- Reconnaissance/serveur.py
+++ Reconnaissance/serveur.py
@@ -69,44 +69,45 @@ while True:
 	client_socket,addr = server_socket.accept()
 	print('GOT CONNECTION FROM:',addr)
 	if client_socket:
-            success, img = cap.read()
-            imgS = imutils.resize(img,width=320)
-            #imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) # redimensionner pour garder les performances
-            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-            a= pickle.dumps(imgS)
-            message = struct.pack("Q",len(a))+a
-            facesCurFrame = face_recognition.face_locations(imgS)
-            encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+            while(cap.isOpened()):
+                success, img = cap.read()
+                #imgS = imutils.resize(img,width=320)
+                imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25) # redimensionner pour garder les performances
+                imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+                a= pickle.dumps(imgS)
+                message = struct.pack("Q",len(a))+a
+                facesCurFrame = face_recognition.face_locations(imgS)
+                encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-            
-            for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-
-                matchIndex = np.argmin(faceDis)
                 
+                for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+                    matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                    faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
 
-                if faceDis[matchIndex]< 0.50:
-                    name = className[matchIndex].upper()
-                    #print(name)
-                    y1,x2,y2,x1 = faceLoc
-                    y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-                    cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-                    cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-                    cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+                    matchIndex = np.argmin(faceDis)
                     
-                else: 
-                    name = 'Unknown'
-                    #print(name)
-                    y1,x2,y2,x1 = faceLoc
-                    y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-                    cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
-                    cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,0,255),cv2.FILLED)
-                    cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
-            
-            out.write(img)
-            cv2.imshow('Webcam',img)
-            key = cv2.waitKey(1) & 0xFF
-            if key ==ord('q'):
-                client_socket.close()
-            cv2.waitKey(1)
+
+                    if faceDis[matchIndex]< 0.50:
+                        name = className[matchIndex].upper()
+                        #print(name)
+                        y1,x2,y2,x1 = faceLoc
+                        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+                        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+                        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+                        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+                        
+                    else: 
+                        name = 'Unknown'
+                        #print(name)
+                        y1,x2,y2,x1 = faceLoc
+                        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+                        cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
+                        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,0,255),cv2.FILLED)
+                        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+                
+                out.write(img)
+                cv2.imshow('Webcam',img)
+                key = cv2.waitKey(1) & 0xFF
+                if key ==ord('q'):
+                    client_socket.close()
+                cv2.waitKey(1)
