34a3de926053a2e5beae1dfccf79ff13957aebd3
Youichii <baurelien@hotmail.com>
2021-10-04 15:17:10 +0200

    fixing nav bar and button bootsrap

--- projet_integration/src/Secretary.js
+++ projet_integration/src/Secretary.js
@@ -3,16 +3,18 @@ import './css/Secretary.css';
 import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-//import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+//import UploadService from "../services/file-upload.service";
 
 
 function Secretary() {
     return (
         <div className="secretary-page">
             <Navbar/>
-            <div>               
-                <Popup trigger={ <button className="add-user">Nouvel utilisateur</button>} position="center" modal nested>
-                    <div>Popup content here !!</div>
+            <div>          
+                <Popup trigger={ <button className="add-user">Ajouter utilisateur (+)</button>} position="center" modal nested>
+                    <div>Choisissez les photos :</div>
+                    <input type="file"/>
                     <button className="save-user">Enregistrer</button>
                 </Popup>
             </div>
