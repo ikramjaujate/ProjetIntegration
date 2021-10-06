2815c2c5888003d9a9b9ffad78d6540c8926fdcd
Youichii <baurelien@hotmail.com>
2021-10-06 17:25:26 +0200

    connecting postgres with node

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/file.controller");
-const app = express()
-const port = 3001
-const model = require('../middleware/model')
+const app = express();
+const port = 3001;
+const knex = require('knex');
+const cors = require('cors');
+require("dotenv").config();
 
 
-
+app.use(cors());
 app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -15,19 +17,31 @@ app.use(function (req, res, next) {
   next();
 });
 
+const db = knex({
+  client: 'pg',
+  connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+  },
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
 
 app.get('/Tables', (req, res) => {
-  model.getTables()
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
+  db.select('*')
+      .from('Client')
+      .then((data) => {
+          console.log(data);
+          res.json(data);
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+});
 
 let routes = (app) => {
   router.post("/upload", controller.upload);
