1a20a9e88debfd41e82d73ae0ac82b827e98433f
Corentin Dallenogare <he201885@students.ephec.be>
2021-10-09 13:00:17 +0200

    tourner le moteur

---
+++ Reconnaissance/moteur.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+
+#Fonction permettant de transformer l'angle en %
+def angle_to_percent (angle) :
+    if angle > 180 or angle < 0 :
+        return False
+
+    start = 4
+    end = 12.5 # car 1°  = +/- 1.75%
+    ratio = (end - start)/180 #Calcul le ratio d'angle en %
+
+    angle_as_percent = angle * ratio
+
+    return start + angle_as_percent
+
+
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setwarnings(False) 
+
+#Utiliser le gpio 12
+pwm_gpio = 12
+frequence = 50
+GPIO.setup(pwm_gpio, GPIO.OUT)
+pwm = GPIO.PWM(pwm_gpio, frequence)
+
+#initialiser a 0°
+pwm.start(angle_to_percent(0))
+time.sleep(1)
+
+#ouvrir la porte ( 110)°
+pwm.ChangeDutyCycle(angle_to_percent(90))
+time.sleep(1)
+
+
+#fermer le gpio
+pwm.stop()
+GPIO.cleanup()
