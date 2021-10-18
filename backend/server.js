fe2341eed2cd029922099ad666810211a5b33423
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-10-18 18:58:46 +0200

    Bonne version du server.js

--- backend/server.js
+++ backend/server.js
@@ -42,21 +42,21 @@ client.connect(err => {
  */ 
 app.get('/api/grades', (request, response) => {
 
-    let query = "select PRM.id, PRM.name, CO.colorCode as color, \
-	   (select count(*) \
-		from camera as CA \
-		join access as AC on CA.id = AC.idCamera \
-	    join profile as PR on AC.idProfile = PR.id \
-	    where PR.id = PRM.id and AC.allowed = true ) as allowedCamera, \
-		\
-		(select count(*) \
-		from camera as CA \
-		join access as AC on CA.id = AC.idCamera \
-	    join profile as PR on AC.idProfile = PR.id \
-	    where PR.id = PRM.id and AC.allowed = false ) as refusedCamera \
-    from profile as PRM \
-    join color as CO on PRM.idColor = CO.idColor \
-    order by PRM.id ;" ;
+    let query = "select GRM.id_grade, GRM.name_grade, CO.name_color as color, \
+                    (select count(*) \
+                    from camera as CA \
+                    join permission as PE on CA.id_camera = PE.id_camera \
+                    join grade as GR on PE.id_grade = GR.id_grade \
+                    where GR.id_grade = GRM.id_grade and PE.allowed = true ) as allowedCamera, \
+                    \
+                    (select count(*) \
+                    from camera as CA \
+                    join permission as PE on CA.id_camera = PE.id_camera \
+                    join grade as GR on PE.id_grade = GR.id_grade \
+                    where GR.id_grade = GRM.id_grade and PE.allowed = false ) as refusedCamera \
+                from grade as GRM \
+                join color as CO on GRM.id_color = CO.id_color \
+                order by GRM.id_grade ;"
 
     client.query(query, (error, results) => {
         if (error) {
@@ -75,10 +75,10 @@ app.get('/api/grades', (request, response) => {
  */ 
 app.get('/api/grades/members', (request, response) => {
 
-    let query = "select PR.id, count(ME.idMember) as members \
-    from profile as PR \
-    left join member as ME on PR.id = ME.idProfile \
-    group by PR.id ;" ;
+    let query = "select GR.id_grade, count(ME.id_member) as members \
+    from grade as GR \
+    left join member as ME on GR.id_grade = ME.id_grade \
+    group by GR.id_grade ;" ;
 
     client.query(query, (error, results) => {
         if (error) {
@@ -100,10 +100,10 @@ app.get('/api/grades/members', (request, response) => {
 
     const idGrade = request.params.idGrade;
 
-    let query = "select idAccess, idProfile, CA.name, allowed, notification \
-    from access as AC \
-    join camera as CA on AC.idCamera = CA.id \
-    where idProfile = ($1)" ;
+    let query = "select PE.id_permission, PE.id_camera, CA.name_camera, allowed, notification \
+    from permission as PE \
+    join camera as CA on PE.id_camera = CA.id_camera \
+    where id_grade = ($1)" ;
     client.query(query, [idGrade], (error, results) => {
         if (error) {
             throw error;
@@ -126,7 +126,7 @@ app.get('/api/grades/members', (request, response) => {
     const name = request.body.name;
     const idColor = request.body.idcolor;
 
-    let query = "insert into profile (name, idColor) \
+    let query = "insert into grade (name_grade, id_color) \
     VALUES (($1), ($2))" ;
     //Requête1 Creer le grade
     client.query(query, [name, idColor], (error, results1) => {
@@ -135,24 +135,24 @@ app.get('/api/grades/members', (request, response) => {
         }
         
         //Requête2 - Récupérer son id
-        let query2 = "select max(profile.id) as idGrade from profile";
+        let query2 = "select max(grade.id_grade) as id_grade from grade;";
         client.query(query2, (error, results2) => {
             if (error) {
                 throw error;
             }
 
             //Requête3 - Récupérer le nombre de caméra maximum
-            let query3 = "select count(*) as numberCamera from camera" ;
+            let query3 = "select count(*) as number_camera from camera;" ;
             client.query(query3, (error, results3) => {
                 if (error) {
                     throw error;
                 }   
 
                 //Requête4 - Créer les actions pour chacune des caméras pour le grade 
-                let query4 = "insert into access (idProfile, idCamera, allowed, notification) \
+                let query4 = "insert into permission (id_grade, id_camera, allowed, notification) \
                 VALUES (($1), ($2), 'false', 'false')" ;
-                let idgrade=results2.rows[0].idgrade ;
-                let nbrcamera = results3.rows[0].numbercamera;
+                let idgrade=results2.rows[0].id_grade ;
+                let nbrcamera = results3.rows[0].number_camera;
                 for (let idCamera=1; idCamera<parseInt(nbrcamera)+1 ; idCamera++) {
                     // try {
                     //     client.query(query4, [idgrade, idCamera], (error, results4) => {
@@ -189,10 +189,10 @@ app.get('/api/grades/members', (request, response) => {
 
     let query = "select * \
     from color \
-    where idColor not in (select CO.idColor \
-                        from color as CO \
-                        join profile as PR on CO.idColor = PR.idColor \
-                        group by CO.idColor, CO.colorCode);" ;
+    where id_color not in (select CO.id_color \
+            from color as CO \
+            join grade as GR on CO.id_color = GR.id_color \
+            group by CO.id_color, CO.name_color);" ;
 
     client.query(query, (error, results) => {
         if (error) {
