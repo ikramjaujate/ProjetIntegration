7c6a0344cb84771956ecfc321baab1a6a3298edf
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-10-18 18:51:00 +0200

    Ajout des states pour rafraichir la valise image

--- frontend/src/components/Camera.js
+++ frontend/src/components/Camera.js
@@ -6,11 +6,8 @@ import { useEffect, useState } from "react";
 
 
 function Camera() {
-    //const [etat, setEtat] = useState("http://0.0.0.0:6060/video")
+    const [etat, setEtat] = useState("http://0.0.0.0:6060/video")
 
-    useEffect(() => {
-        document.getElementById("changement").innerHTML = `<img id ="test" alt="video surveillance" src=ikram2.jpg width="640" height="480" />`
-    }, [])
     function Allumer() {
         console.log("up")
         fetch("http://0.0.0.0:6060/up", {
@@ -19,21 +16,17 @@ function Camera() {
             },
             mode: 'no-cors'
         }).then(res => res.json).then(data => {
-            
-            document.getElementById("changement").innerHTML = `<img id ="test" alt="video surveillance" src="http://0.0.0.0:6060/video" width="640" height="480" />`
-
-            console.log("success")
+            setEtat("http://0.0.0.0:6060/video")
         })
-
-        //window.location = "/camera"
-
     }
+
     function Capture() {
         //event.preventDefault()
         console.log("capture")
         fetch("http://0.0.0.0:6060/photo")
 
     }
+
     function Eteindre() {
         console.log("éteindre")
         fetch("http://0.0.0.0:6060/shutdown", {
@@ -42,10 +35,8 @@ function Camera() {
             },
             mode: 'no-cors'
         }).then(res => res.json).then(data => {
-            document.getElementById("changement").innerHTML = `<img alt="video surveillance" src=ikram1.jpg width="640" height="480" />`
-            console.log("success")
+            setEtat('camera-disconnected.png')
         })
-
     }
 
     return (
@@ -53,21 +44,16 @@ function Camera() {
             <div class="row align-items-center">
                 <div class="m-auto px-2">
                     <h1>test</h1>
-
                     <section class="streaming">
                         <h3>Camera</h3>
                         <div id="changement">
-
-                            {/*<img alt="video surveillance" src={"ikram2.jpg"} width="640" height="480" />*/}
-                            {/*<img id ="test"alt="video surveillance" src="http://0.0.0.0:6060/video" width="640" height="480" />*/}
+                            <img id ="test"alt="video surveillance" src={etat} width="640" height="480" />
                         </div>
                     </section>
-
 
                     <button class="btn btn-primary m-5" type="button" onClick={Capture}>prend photo</button>
                     <button class="btn btn-primary m-5" onClick={Allumer} type="button" name="start" value="Eteindre">up</button>
                     <button class="btn btn-primary m-5" onClick={Eteindre} type="button" name="stop" value="Eteindre">Éteindre</button>
-
 
                 </div >
             </div>
