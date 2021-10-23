b7bde741e0de5cc17908701397d4419c76bf81d1
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-10-23 13:19:28 +0200

    Ajout des API dans les fichiers
    API utilisée dans les fichiers par catégories, pour plus de clarté.
    
    Co-Authored-By: clementinesacre <sacre.clementine@gmail.com>

---
+++ backend/back-cameras.js
@@ -0,0 +1,19 @@
+
+ module.exports = function(app,client) {
+     /**
+ * Récupère à l'aide d'un GET toutes les caméra et leur état 
+ * @author Cécile Bonnet <c.bonnet@gmail.com>
+ * @method GET
+ **/
+
+ app.get('/api/etatCam', (req, res) =>{
+  
+    client.query('select * from camera', (err, result) => {
+      console.log(result.rows)
+  
+      if(err) throw err ;
+      res.send(result.rows);
+     
+    })
+  })
+ }
