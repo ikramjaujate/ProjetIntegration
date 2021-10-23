2292253c5c796ea2aba6a4221c2fc54c08b92726
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-10-23 14:40:50 +0200

    Ajout nouvelles API pour get information user et photos

--- backend/routes/back-members.js
+++ backend/routes/back-members.js
@@ -5,63 +5,108 @@
  * @method : PUT
  * 
  */
- module.exports = function(app,client) {
- app.put('/api/client', (req, res) => {  
+module.exports = function (app, client) {
+  app.put('/api/client', (req, res) => {
     const firstName = req.body.FirstName
     const lastName = req.body.LastName
     const grade = req.body.Grade
     console.log(grade)
-    let query = 'insert into member (id_grade, first_name, last_name) values (($1), ($2), ($3))' ;
+    let query = 'insert into member (id_grade, first_name, last_name) values (($1), ($2), ($3))';
     client.query(query, [grade, firstName, lastName], (error, result) => {
-        
+
     })
   })
-  
- /**
- * 
- * @author : Aurélien
- * @method : GET
- * 
- */
- 
+
+  /**
+  * 
+  * @author : Aurélien
+  * @method : GET
+  * 
+  */
+
   app.get('/api/gradesInfos', (request, response) => {
     let query = "select id_grade, name_grade from grade";
-    
+
     client.query(query, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        response.status(200).json(results.rows);
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     })
-  }) ;
-  
-  
-/**
- * 
- * @author : Aurélien
- * 
- */
-
- /*let routes = (app) => {
-    router.post("/upload", controller.upload);
-    router.get("/files", controller.getListFiles);
-    router.get("/files/:name", controller.download);
-  
-    app.use(router);
-  };
-  
-  module.exports = routes;
-global.__basedir = __dirname;
-
-const initRoutes = require("./src/routes");
-
-app.use(express.urlencoded({ extended: true }));
-initRoutes(app);
-
-let port2 = 8080;  //listen on port 8080 for incoming requests.
-app.listen(port, () => {
-  console.log(`Running at localhost:${port2}`);
-});*/
+  });
 
 
- }
+  /**
+   * 
+   * @author : Aurélien
+   * 
+   */
+
+  /*let routes = (app) => {
+     router.post("/upload", controller.upload);
+     router.get("/files", controller.getListFiles);
+     router.get("/files/:name", controller.download);
+   
+     app.use(router);
+   };
+   
+   module.exports = routes;
+ global.__basedir = __dirname;
+ 
+ const initRoutes = require("./src/routes");
+ 
+ app.use(express.urlencoded({ extended: true }));
+ initRoutes(app);
+ 
+ let port2 = 8080;  //listen on port 8080 for incoming requests.
+ app.listen(port, () => {
+   console.log(`Running at localhost:${port2}`);
+ });*/
+
+  /**
+   * Retrieves all the information from a member
+   * 
+   * @author Ikram Jaujate Ouldkhala <i.jaujateouldkhala@students.ephec.be>
+   * @method GET
+   * @param {integer} idMember identifier of the member for which we want to retrieve information
+   */
+
+  app.get('/api/membres/:idMember', (request, response) => {
+
+    const idMember = request.params.idMember;
+
+    let query = "select *  \
+  from member \
+  where id_member = ($1)" ;
+    client.query(query, [idMember], (error, results) => {
+      if (error) {
+      }
+      response.status(200).json(results.rows)
+    })
+  })
+
+  /**
+   * Retrieves only the pictures from a member
+   * 
+   * @author Ikram Jaujate Ouldkhala <i.jaujateouldkhala@students.ephec.be>
+   * @method GET
+   * @param {integer} idMember identifier of the member for which we want to retrieve the picture
+   */
+  app.get('/api/membres/:idMember/photos', (request, response) => {
+
+    const idMember = request.params.idMember;
+
+    let query = "select PO.pictures  \
+  from photos as PO\
+  join member as ME on PO.id_member = ME.id_member  \
+  where ME.id_member = ($1)" ;
+    client.query(query, [idMember], (error, results) => {
+      if (error) {
+      }
+      response.status(200).json(results.rows)
+    })
+  })
+
+
+
+}
