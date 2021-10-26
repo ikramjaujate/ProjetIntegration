e7b4b1b75390842339026f6088b1a925a27ab2ac
Clémentine <sacre.clementine@gmail.com>
2021-10-26 10:36:49 +0200

    Workflow lintage
    Essai de lancement du linter automatiquement lorsque la branche est push

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,54 @@
-const app = express() ;
-const {Client}= require('pg') ;
+const express = require('express')
+const app = express()
+const { Client } = require('pg')
 
-const http = require('http');
-const dotenv = require('dotenv');
-dotenv.config();
-const express = require('express') ;
-const cors = require('cors') ;
-const mysql = require('mysql');
-const router = express.Router();
-const controller = require("./src/controller/file.controller");
-const { request, response } = require('express');
-const port = 3001;
+// const http = require('http')
+const dotenv = require('dotenv')
+dotenv.config()
+// const cors = require('cors')
+// const mysql = require('mysql')
+// const router = express.Router()
+// const controller = require('./src/controller/file.controller')
+// const { request, response } = require('express')
+const port = 3001
 
 /* Different sets of existing APIs */
-var grade =  require('./routes/back-grade.js');
-var members = require('./routes/back-members.js');
-var privatedata = require('./routes/back-privatedata.js');
-var cameras = require('./routes/back-cameras.js');
-
+const grade = require('./routes/back-grade.js')
+const members = require('./routes/back-members.js')
+const privatedata = require('./routes/back-privatedata.js')
+const cameras = require('./routes/back-cameras.js')
 
 const client = new Client({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-  })
-
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+})
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+  console.log(`App running on port ${port}.`)
+})
 
-app.use(express.json()) ;
+app.use(express.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    next();
-  });
-
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 
 client.connect(err => {
   if (err) {
-    console.error('connection error', err.stack);
-  } 
-  else {
-    console.log('connected');
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
   }
-}) ;
+})
 
-// ROUTE FOR API 
-grade(app, client);
-cameras(app, client);
-members(app, client);
-privatedata(app,client);
-
-
-
-
+// ROUTE FOR API
+grade(app, client)
+cameras(app, client)
+members(app, client)
+privatedata(app, client)
