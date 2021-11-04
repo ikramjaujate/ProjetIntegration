a576aff03143edf4cf5afe3baba7d6f6404f1fff
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-11-04 20:54:33 +0100

    Résolution bug .env et build
    Co-Authored-By: clementinesacre <sacre.clementine@gmail.com>
    Co-Authored-By: corentinDallenogare <corentda@hotmail.fr>

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@ echo "Creation de l'Application a déployer"
 mkdir ./Application
 mkdir ./Application/www
 
+cp -R backend ./Application/www
 cd frontend
 npm run build
+mv ./build ../Application/www/backend
+
 cd ..
-cp -R backend ./Application/www
-cp -R frontend/build ./Application/www/backend
+
 rm -rf ./Application/www/backend/node_modules 
 rm -rf ./Application/www/backend/.env 
 
 cp -R db ./Application
 
-cp .env ./Application/www/backend
+cp ./.env ./Application/www/backend
 
 cp docker/docker-compose.yml ./Application/
 
