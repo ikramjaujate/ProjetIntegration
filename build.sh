edab167f445be30c820c49974196a64d01d4818d
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-11-04 11:10:44 +0100

    Resolution problème indentation

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 echo "Creation de l'Application a déployer"
-mkdir Application
-mkdir Application/www
+mkdir ./Application
+mkdir ./Application/www
 
 cd frontend
 npm run build
 cd ..
-cp -R backend Application/www
-mv frontend/build Application/www/backend
-rm -rf Application/www/backend/node_modules 
-rm -rf Application/www/backend/.env 
+cp -R backend ./Application/www
+mv frontend/build ./Application/www/backend
+rm -rf ./Application/www/backend/node_modules 
+rm -rf ./Application/www/backend/.env 
 
-cp -R db Application
+cp -R db ./Application
 
-cp .env Application/www/backend
+cp .env ./Application/www/backend
 
-cp docker/docker-compose.yml Application/
+cp docker/docker-compose.yml ./Application/
 
 echo 'Success'
