0132ee83f23e9ceb3c570df3f0377496dbfcc6b9
Corentin Dallenogare <he201885@students.ephec.be>
2021-11-04 13:15:15 +0100

    suppression foncton inutile

--- backend/Camera-1/moteur.py
+++ backend/Camera-1/moteur.py
@@ -15,22 +15,6 @@ import RPi.GPIO as GPIO
 import time
 
 
-
-#Set function to calculate percent from angle
-def angle_to_percent (angle) :
-    if angle > 180 or angle < 0 :
-        return False
-
-    start = 4
-    end = 12.5
-    ratio = (end - start)/180 #Calcul ratio from angle to percent
-
-    angle_as_percent = angle * ratio
-
-    return start + angle_as_percent
-
-
-
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 path = './images'
