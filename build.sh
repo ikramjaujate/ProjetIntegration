20ed3142d3be4baa534a239a0d7e19c7d03571e7
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-11-04 21:02:30 +0100

    Debug .env et build
    Co-Authored-By: clementinesacre <sacre.clementine@gmail.com>
    Co-Authored-By: corentinDallenogare <corentda@hotmail.fr>

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 echo "Creation de l'Application a déployer"
+cd frontend
+npm run build
+cd ..
 mkdir ./Application
 mkdir ./Application/www
 
 cp -R backend ./Application/www
-cd frontend
-npm run build
-cp -R ./build ../Application/www/backend
-ls -a 
 
-
-cd ..
+cp -R frontend/build ./Application/www/backend
 
 rm -rf ./Application/www/backend/node_modules 
 rm -rf ./Application/www/backend/.env 
 
 cp -R db ./Application
 
+ls -a Application/db
 cp ./.env ./Application/www/backend
 
 cp docker/docker-compose.yml ./Application/
 
-ls -a Application/www/backend
 echo 'Success'
 
