b5eb196a852f2ef794817a8a238a8b8c3ef21c36
Clémentine <sacre.clementine@gmail.com>
2021-11-13 10:39:51 +0100

    Ajout de directives CSP convenables
    Qui permettrait au site de fournir les documents

--- backend/server.js
+++ backend/server.js
@@ -47,16 +47,16 @@ app.use(helmet());
 //     action: "deny",
 //   })
 //  );
-// app.use(helmet.contentSecurityPolicy({
-//   directives: {
-//     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-//     'default-src': ['\'self\'', 'https://restcountries.eu', 'blob:'],
-//     'object-src' : ['\'self\'', 'https://restcountries.eu', 'data:'],
-//     'img-src' : ['\'self\'', 'https://restcountries.eu', 'data:'],
-//     'script-src' : ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\''],
-//     'script-src-attr': ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\''],
-//   }
-// }));
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+    'default-src': ['\'self\'', 'https://restcountries.eu', 'blob:'],
+    'object-src' : ['\'self\'', 'https://restcountries.eu', 'data:'],
+    'img-src' : ['\'self\'', 'https://restcountries.eu', 'data:'],
+    'script-src' : ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\''],
+    'script-src-attr': ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\''],
+  }
+}));
 // app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }));
 // app.use(
 //   helmet.hsts({
