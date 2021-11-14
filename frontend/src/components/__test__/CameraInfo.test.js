43f9e5e5406a167ba8624084576b9c1a8294d307
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-11-14 09:57:52 +0100

    tests unitaires page cameraInfo

---
+++ frontend/src/components/__test__/CameraInfo.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16' ;
+import 'jest-enzyme' 
+import 'jest-styled-components'
+import CameraInfo from '../CameraInfo.js' ;
+Enzyme.configure({ adapter: new Adapter() })
+
+
+describe('CameraInfo', () => {
+    it('Vérifier le nom de la camera passé en paramètre. ', () => {
+      const wrapper = shallow(<CameraInfo name="Lounge" allowed="" notification=""/>)
+      const paragraph = wrapper.find('div.test2')
+      expect(paragraph).toHaveLength(1)
+      expect(paragraph.text()).toEqual('Lounge')
+    })
+
+    it('Vérifier que le booleen passé en param dans allowed renvoie le bon backgroud (1er condition).', () => {
+      const wrapper = shallow(<CameraInfo name="" allowed="" notification=""/>)
+      const paragraph = wrapper.find('div.test3')
+      expect(paragraph).toHaveLength(1)
+      expect(paragraph.prop('style')).toHaveProperty("backgroundColor","var(--camera-refuse)")
+    })
+
+    it('Vérifier que le booleen passé en param dans allowed renvoie le bon backgroud (2eme condition).', () => {
+        const wrapper = shallow(<CameraInfo name="" allowed="true" notification=""/>)
+        const paragraph = wrapper.find('div.test3')
+        expect(paragraph).toHaveLength(1)
+        expect(paragraph.prop('style')).toHaveProperty("backgroundColor","var(--camera-allow)")
+    })
+
+    it('Vérifier que le booleen passé en param dans notification renvoie la bonne couleur (1ere condition).', () => {
+        const wrapper = shallow(<CameraInfo name="" allowed="" notification="true"/>)
+        const paragraph = wrapper.find('i.test4')
+        expect(paragraph).toHaveLength(1)
+        expect(paragraph.prop('style')).toHaveProperty("color","white")
+    })
+
+    it('Vérifier que la couleur passé en param dans notification renvoie la bonne couleur (2eme condition).', () => {
+        const wrapper = shallow(<CameraInfo name="" allowed="true" notification=""/>)
+        const paragraph = wrapper.find('i.test4')
+        expect(paragraph).toHaveLength(1)
+        expect(paragraph.prop('style')).toHaveProperty("color","var(--camera-allow)")
+    })
+
+    it('Vérifier que la couleur passé en param dans notification renvoie la bonne couleur (3eme condition).', () => {
+        const wrapper = shallow(<CameraInfo name="" allowed="" notification=""/>)
+        const paragraph = wrapper.find('i.test4')
+        expect(paragraph).toHaveLength(1)
+        expect(paragraph.prop('style')).toHaveProperty("color","var(--camera-refuse)")
+    })  
+})
