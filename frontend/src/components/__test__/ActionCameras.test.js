9b9397c4cb8af7f33f5d4f0adec512eb3948a818
Clémentine <sacre.clementine@gmail.com>
2021-11-15 10:27:18 +0100

    Correction des noms de variables
    Pour que ce soit plus clair de ce qu'on est en train de tester

--- frontend/src/components/__test__/ActionCameras.test.js
+++ frontend/src/components/__test__/ActionCameras.test.js
@@ -12,84 +12,84 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('ActionCameras', () => {
     it('Snapshot - renders correctement', () => {
-      const paragraph = renderer.create(<ActionsCameras allowed={false} id_camera="1" currentIdGrade="1"/>).toJSON();
-      expect(paragraph).toMatchSnapshot();
+      const snapshot = renderer.create(<ActionsCameras allowed={false} id_camera="1" currentIdGrade="1"/>).toJSON();
+      expect(snapshot).toMatchSnapshot();
     });
 
     it('Vérifier le texte affiché dans le titre', () => {
       const wrapper = shallow(<ActionsCameras name_camera="ZoneSud" />);
-      const paragraph = wrapper.find('div.name-camera-grade');
+      const nameCamera = wrapper.find('.name-camera-grade');
       //expect(paragraph).toHaveLength(1);
-      expect(paragraph.text()).toEqual('ZoneSud');
+      expect(nameCamera.text()).toEqual('ZoneSud');
     })
 
     it("Vérifier que le switch représentant l'action de la caméra est activé", () => {
       const wrapper = shallow(<ActionsCameras allowed={true} />);
-      const paragraph = wrapper.find('input.form-check-input');
-      expect(paragraph.at(0).props().defaultChecked).toEqual(true);
+      const check = wrapper.find('.form-check-input');
+      expect(check.at(0).props().defaultChecked).toEqual(true);
     })
 
     it("Vérifier que le switch représentant l'action de la caméra est désactivé", () => {
       const wrapper = shallow(<ActionsCameras allowed={false} />);
-      const paragraph = wrapper.find('input.form-check-input');
-      expect(paragraph.at(0).props().defaultChecked).toEqual();
+      const check = wrapper.find('.form-check-input');
+      expect(check.at(0).props().defaultChecked).toEqual();
     })
 
     it("Vérifier la classe du switch représentant l'action de la caméra", () => {
       const wrapper = shallow(<ActionsCameras id_camera="2" currentIdGrade="1" />);
-      const paragraph = wrapper.find('input.form-check-input');
-      expect(paragraph.hasClass('action-1-2')).toEqual(true) ;
+      const check = wrapper.find('.form-check-input');
+      expect(check.hasClass('action-1-2')).toEqual(true) ;
     })
 
     it("Vérifier l'id de la notification", () => {
       const wrapper = shallow(<ActionsCameras id_camera="2" currentIdGrade="1" />);
-      const paragraph = wrapper.find('.bi');
-      expect(paragraph.at(0).props().id).toEqual("notification-1-2") ;
+      const notification = wrapper.find('.bi');
+      expect(notification.at(0).props().id).toEqual("notification-1-2") ;
     })
 
 
     it('Vérifier le texte affiché dans le titre a une valeur par défaut', () => {
       const wrapper = shallow(<ActionsCameras />);
-      const paragraph = wrapper.find('.name-camera-grade');
-      expect(paragraph.text()).toEqual('Chargement');
+      const nameCamera = wrapper.find('.name-camera-grade');
+      expect(nameCamera.text()).toEqual('Chargement');
     })
 
     it("Vérifier la valeur par défaut du switch représentant l'action de la caméra", () => {
       const wrapper = shallow(<ActionsCameras />);
-      const paragraph = wrapper.find('.form-check-input');
-      expect(paragraph.at(0).props().defaultChecked).toEqual();
+      const check = wrapper.find('.form-check-input');
+      expect(check.at(0).props().defaultChecked).toEqual();
     })
 
     it("Vérifier la classe par défaut  du switch représentant l'action de la caméra", () => {
       const wrapper = shallow(<ActionsCameras />);
-      const paragraph = wrapper.find('.form-check-input');
-      expect(paragraph.hasClass('action-0-0')).toEqual(true) ;
+      const check = wrapper.find('.form-check-input');
+      expect(check.hasClass('action-0-0')).toEqual(true) ;
     })
 
     it("Vérifier l'id par défaut de la notification", () => {
       const wrapper = shallow(<ActionsCameras />);
-      const paragraph = wrapper.find('.bi');
-      expect(paragraph.at(0).props().id).toEqual("notification-0-0") ;
+      const notification = wrapper.find('.bi');
+      expect(notification.at(0).props().id).toEqual("notification-0-0") ;
     })
 
     it("Vérifie qu'il y a bien un fonction par défaut pour le changeAction", () => {
        const wrapper = shallow(<ActionsCameras />);
-       const paragraph = wrapper.find('.form-check-input');
-       paragraph.simulate('change') ;
+       const fctCheck = wrapper.find('.form-check-input');
+       fctCheck.simulate('change') ;
     });
 
     it("Vérifie qu'il y a bien un fonction par défaut pour le changeNotification", () => {
       const wrapper = shallow(<ActionsCameras />);
-      const paragraph = wrapper.find('.bi-bell-slash-fill');
-      paragraph.simulate('click') ;
+      const fctNotification = wrapper.find('.bi-bell-slash-fill');
+      fctNotification.simulate('click') ;
    });
     // it('`button` should be disabled', () => {
     //   const wrapper = shallow(<ActionsCameras name_camera="" id_camera="" notification="" changeAction="" changeNotification="" allowed={false} currentIdGrade="" />);
-    //   const paragraph = wrapper.find('.form-check-input');
-    //   console.log("avant : ", paragraph.at(0).props())
-    //   paragraph.simulate('change') ;
-    //   console.log("apres : ",paragraph.at(0).props().checked )
-    //   expect(paragraph.at(0).props().ischecked).toEqual(true);
+    //   const fctCheck = wrapper.find('.form-check-input');
+    //   console.log("avant : ", fctCheck.at(0).props())
+    //   fctCheck.simulate('change') ;
+    //   console.log("apres : ",fctCheck.at(0).props().checked )
+    //   expect(fctCheck.at(0).props().ischecked).toEqual(true);
     // });
 })
   
--- frontend/src/components/__test__/TitleModalGrade.test.js
+++ frontend/src/components/__test__/TitleModalGrade.test.js
@@ -11,33 +11,33 @@ Enzyme.configure({ adapter: new Adapter() }); //Configurer Enzyme pour react 16
 
 describe('TitleModalGrade', () => {
     it('Snapshot - renders correctement', () => {
-      const paragraph = renderer.create(<TitleModalGrade text="Test" bgColor="#000000"/>).toJSON();
-      expect(paragraph).toMatchSnapshot();
+      const snapshot = renderer.create(<TitleModalGrade text="Test" bgColor="#000000"/>).toJSON();
+      expect(snapshot).toMatchSnapshot();
     });
 
     it('Vérifier le texte affiché dans le titre', () => {
       const wrapper = shallow(<TitleModalGrade text="Directeur" />);
-      const paragraph = wrapper.find('h5');
-      expect(paragraph).toHaveLength(1);
-      expect(paragraph.text()).toEqual('Directeur');
+      const title = wrapper.find('h5');
+      expect(title).toHaveLength(1);
+      expect(title.text()).toEqual('Directeur');
     })
       
     it('Vérifier la couleur du fond du titre', () => {
       const wrapper = shallow(<TitleModalGrade bgColor="#086589"/>);
-      const paragraph = wrapper.find('.modal-title');
-      expect(paragraph.prop('style')).toHaveProperty("backgroundColor", "#086589");
+      const modalTitle = wrapper.find('.modal-title');
+      expect(modalTitle.prop('style')).toHaveProperty("backgroundColor", "#086589");
     })
 
     it('Vérifier que le props bgColor a une valeur par défaut', () => {
       const wrapper = shallow(<TitleModalGrade/>);
-      const paragraph = wrapper.find('.modal-title');
-      expect(paragraph.prop('style')).toHaveProperty("backgroundColor", 'var(--text-loading)');
+      const modalTitle = wrapper.find('.modal-title');
+      expect(modalTitle.prop('style')).toHaveProperty("backgroundColor", 'var(--text-loading)');
     })
 
     it('Vérifier que le props text a une valeur par défaut', () => {
       const wrapper = shallow(<TitleModalGrade/>);
-      const paragraph = wrapper.find('h5');
-      expect(paragraph.text()).toEqual('Chargement');
+      const title = wrapper.find('h5');
+      expect(title.text()).toEqual('Chargement');
     })
 })
   
