13eaaaa4fb509e095ae868e584e838500ac2531c
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-11-20 18:53:28 +0100

    Combinaison des pages accueil et de ses fonctionnalités
    Co-Authored-By: clementinesacre <sacre.clementine@gmail.com>

--- frontend/src/components/Live.js
+++ frontend/src/components/Live.js
@@ -10,7 +10,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-function Live(){
+function Live(etat){
   
   
 
@@ -23,7 +23,7 @@ return(
             <button type="button" class=" btn-secondary btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <img className="img-thumbnail" alt='visu_live'src= "http://0.0.0.0:6060/video" width="440" height="300" title="Foscam FI8905W" />
+            <img className="img-thumbnail" alt='visu_live' src={etat} width="440" height="300" title="Foscam FI8905W" />
           </div>
           <div class="modal-footer justify-content-center">
             <button type="button" className=" col-3 btn btn-secondary"><i className="bi bi-camera-reels"></i></button>
--- frontend/src/components/ToggleSwitch.js
+++ frontend/src/components/ToggleSwitch.js
@@ -1,20 +1,63 @@
 import React from "react";
 import "../css/ToggleSwitch.css";
+import { useEffect, useState } from "react";
   
-const ToggleSwitch = ({ nom,status,id  }) => {
+const ToggleSwitch = ({ nom,status,id , setEtat }) => {
+  //const [etat, setEtat] = useState("http://0.0.0.0:6060/video")
+  function Allumer() {
+    console.log("up")
+    fetch("http://0.0.0.0:6060/up", {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        mode: 'no-cors'
+    }).then(res => res.json).then(data => {
+        setEtat("http://0.0.0.0:6060/video")
+    })
+}
+function Eteindre() {
+  console.log("éteindre")
+  fetch("http://0.0.0.0:6060/shutdown", {
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      mode: 'no-cors'
+  }).then(res => res.json).then(data => {
+      setEtat('camera-disconnected.png')
+  })
+}
+  function changeStatusCam(identifiant) {
+    let statut = document.getElementById(identifiant).checked
+    if (statut) {
+        Allumer();
+    }
+    else {
+        Eteindre();
+    }
+}
     
   return (
     <div className='col-2 col-md-2 col-sm-2 offset-4 offset-md-5 offset-sm-4 offset-lg-7 switchcam'>
-      <div className="toggle-switch">
+      {/*<div className="toggle-switch">
         {status===1?
-            <input type="checkbox" className="checkbox" id={id} defaultChecked  /> : status===2? <input type="checkbox" className="checkbox" id={id} />:<input type="checkbox" className="checkbox" id={nom} disabled style={{'backgroundColor':'grey'}} />
+            <input type="checkbox" className="checkbox" id={id} defaultChecked onClick={() => changeStatusCam(id)}  /> : status===2? <input type="checkbox" className="checkbox" id={id} onClick={() => changeStatusCam(id)} />:<input type="checkbox" className="checkbox" id={nom} disabled style={{'backgroundColor':'grey'}} />
         }
         <label className="label" htmlFor={nom}>
           <span className="inner" />
           <span className="switch" />
         </label>
-      </div>
+      </div>*/}
+
+                        <div class="form-check form-switch">
+                            {status === 1 ? <input class="form-check-input" type="checkbox" role="switch" id={id} defaultChecked onClick={() => changeStatusCam(id)} /> :
+                                status === 2 ? <input class="form-check-input" type="checkbox" role="switch" id={id} onClick={() => changeStatusCam(id)} /> :
+                                    <input class="form-check-input" type="checkbox" role="switch" id={id} disabled />}
+                        </div>
+                    
     </div>
+
+
+
   );
 };
   
