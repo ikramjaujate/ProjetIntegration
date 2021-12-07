7b4282b9f7737878008d3e4dde420316bb200b63
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-12-07 12:16:06 +0100

    Ajout correction

--- db/fichier.sql
+++ db/fichier.sql
@@ -1,7 +1,7 @@
-
+CREATE USER pW95V2zYwk4L WITH PASSWORD 'Am6Mm5BW35bw';
 CREATE DATABASE ProjetIntegration;
-ALTER USER postgres WITH PASSWORD '123';
-GRANT ALL PRIVILEGES ON DATABASE ProjetIntegration TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE ProjetIntegration to pW95V2zYwk4L;
+
 
 \c ProjetIntegration;
 
@@ -13,14 +13,13 @@ CREATE TABLE IF NOT EXISTS public.status
 );
 
 ALTER TABLE public.status
-    OWNER to postgres;
-	
+    OWNER to pW95V2zYwk4L;
+
 INSERT INTO status(name_status)
-VALUES 
+VALUES
 ('ON'),
 ('OFF'),
 ('DISCONNECTED');
-
 
 
 CREATE TABLE IF NOT EXISTS public.camera
@@ -33,35 +32,34 @@ CREATE TABLE IF NOT EXISTS public.camera
 );
 
 ALTER TABLE public.camera
-    OWNER to postgres;
-	
+    OWNER to pW95V2zYwk4L;
+
 INSERT INTO camera(name_camera, id_status)
-VALUES 
+VALUES
 ('CAFET', 1),
 ('LOUNGE', 1),
 ('SREU1', 3),
 ('SREU2', 1),
 ('SREU3', 1),
 ('SERVERROOM', 2),
-('ACCUEIL', 2),
+('ACCEUIL', 2),
 ('WC', 1),
 ('ENTREESUD', 1),
 ('ENTRENORD', 3);
 
 
-
 CREATE TABLE IF NOT EXISTS public.color
 (
     id_color SERIAL NOT NULL,
-    name_color varchar(25) NOT NULL,
+    name_color char(25) NOT NULL,
     PRIMARY KEY (id_color)
 );
 
 ALTER TABLE public.color
-    OWNER to postgres;
+    OWNER to pW95V2zYwk4L;
 
 INSERT INTO color(name_color)
-VALUES 
+VALUES
 ('#B2DFDB'),
 ('#F8BBD0'),
 ('#BBDEFB'),
@@ -92,13 +90,13 @@ CREATE TABLE IF NOT EXISTS public.grade
 );
 
 ALTER TABLE public.grade
-    OWNER to postgres;	
-	
+    OWNER to pW95V2zYwk4L;
+
 INSERT INTO grade(name_grade, id_color)
-VALUES 
+VALUES
 ('Directeur', 10),
 ('Personnel', 18),
-('Bénéficiaire', 7);  
+('Bénéficiaire', 7);
 
 
 CREATE TABLE IF NOT EXISTS public.permission
@@ -114,10 +112,10 @@ CREATE TABLE IF NOT EXISTS public.permission
 );
 
 ALTER TABLE public.permission
-    OWNER to postgres;
-	
+    OWNER to pW95V2zYwk4L;
+
 INSERT INTO permission(id_grade, id_camera, allowed, notification)
-VALUES 
+VALUES
 (1, 1, true, false),
 (1, 2, true, false),
 (1, 3, true, false),
@@ -154,17 +152,17 @@ CREATE TABLE IF NOT EXISTS public.member
 (
     id_member SERIAL NOT NULL,
     id_grade INTEGER NOT NULL,
-	first_name VARCHAR(25) NOT NULL,
-	last_name VARCHAR(25) NOT NULL,
+	first_name varchar(25) NOT NULL,
+	last_name varchar(25) NOT NULL,
     PRIMARY KEY (id_member),
 	FOREIGN KEY (id_grade) REFERENCES grade(id_grade)
 );
 
 ALTER TABLE public.member
-    OWNER to postgres;
-	
+    OWNER to pW95V2zYwk4L;
+
 INSERT INTO member(id_grade, first_name, last_name)
-VALUES 
+VALUES
 (1, 'Jean', 'Ab'),
 (3, 'Louise', 'Cd'),
 (3, 'Marie', 'Ef'),
@@ -174,9 +172,9 @@ VALUES
 (3, 'Eva', 'Mn'),
 (3, 'Clara', 'Op'),
 (3, 'Marion', 'Qr'),
-(2, 'Théo', 'St'),
+(2, 'Theo', 'St'),
 (2, 'Julien', 'Uv'),
-(3, 'Clément', 'Wx'),
+(3, 'Clement', 'Wx'),
 (3, 'Laurent', 'Yz'),
 (3, 'Laurence', 'Az'),
 (3, 'Pilou', 'By');
@@ -185,27 +183,29 @@ VALUES
 CREATE TABLE public.photos
 (
     id_member SERIAL NOT NULL,
-    pictures VARCHAR(1024) NOT NULL,
+    pictures CHAR(1024) NOT NULL,
 	FOREIGN KEY (id_member) REFERENCES member(id_member)
 );
 
 INSERT INTO photos(id_member, pictures)
-VALUES 
+VALUES
 (1, 'ikram1.jpg'),
-(1, 'ikram2.jpg');
-	
+(1, 'ikram2.jpg'),
+(2, 'corentin1.jpg'),
+(2, 'corentin2.jpg');
+
 
 CREATE TABLE IF NOT EXISTS public.personal
 (
     id_personal SERIAL NOT NULL,
     username varchar(25) NOT NULL,
-    password varchar(1024) NOT NULL,
+    password varchar(255) NOT NULL,
     PRIMARY KEY (username)
 );
 
 ALTER TABLE public.personal
-    OWNER to postgres;
+    OWNER to pW95V2zYwk4L;
 
 INSERT INTO personal(username, password)
-VALUES 
+VALUES
 ('toto', 'toto');
