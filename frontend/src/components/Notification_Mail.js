dc775d9b59745cdc8fba6cb739ade32f971c37bc
juliefino <j.fino@students.ephec.be>
2021-12-08 14:59:39 +0100

    creation function email + template

---
+++ frontend/src/components/Notification_Mail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
+
+
+function sendMail() {
+
+    init("user_LHFMoI6uv3conWaZcO9j6");
+    emailjs.send("service_mail", "template_cajacci", {
+        grade: "toto",
+    }).then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+}
+
+sendMail()
