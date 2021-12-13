29b52ac7c1f91ad0777f0ecd9e9792bfd332291c
Youichii <baurelien@hotmail.com>
2021-12-13 18:54:11 +0100

    Update app.py

--- backend/Reconnaissance/app.py
+++ backend/Reconnaissance/app.py
@@ -171,6 +171,12 @@ def gen(captur):
                 cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
                 cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
                 cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+                # response = requests.get('https://google.com/')
+                # if response:
+                #     pass
+                # elif response == False:
+                #     sendNotificatyion()
+
 
                 
                 
