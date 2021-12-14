94d29c77564666892f3c1c70f22a3f26c7c33888
juliefino <j.fino@students.ephec.be>
2021-12-14 18:12:31 +0100

    jsdoc fonction sendMail

--- frontend/src/components/Notification_Mail.js
+++ frontend/src/components/Notification_Mail.js
@@ -3,16 +3,20 @@ import emailjs from 'emailjs-com';
 import{ init } from 'emailjs-com';
 
 
-function sendMail() {
-    
-    /* jsdoc
-   
-   
-    */
+/** 
+     * Sends an email with the grade of the person that's in front of the camera
+     *
+     * @author juliefino
+     * 
+     * @param {string} grade    Grade's name 
+     * @return the email sending  
+     */
+
+function sendMail(grade_personne) {
 
     init("user_LHFMoI6uv3conWaZcO9j6");
     emailjs.send("service_mail", "template_cajacci", {
-        grade: "toto",
+        grade: grade_personne,
     }).then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -21,3 +25,7 @@ function sendMail() {
 }
 
 sendMail()
+
+
+
+//emailjs.send("service_mail","template_anonyme");
