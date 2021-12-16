05729db5c8fae3119ebbcdfec42159a5a320e690
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-12-16 10:30:25 +0100

    modal

---
+++ frontend/src/components/ModalLive.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import'bootstrap/dist/css/bootstrap.min.css';
+import'bootstrap/dist/js/bootstrap.min.js';
+import'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import {useState} from "react" ;
+
+
+function testModal() {
+
+    return (
+        <>
+            
+
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default testModal; 
--- frontend/src/components/OngletCamera.js
+++ frontend/src/components/OngletCamera.js
@@ -1,29 +1,58 @@
- import '../css/OngletCamera.css';
- 
- import ToggleSwitch from "../components/ToggleSwitch.js";
- import Live from "../components/Live";
- import React from 'react';
- import { useEffect, useState } from "react";
+import '../css/OngletCamera.css';
 
-function CadreCameras ({idCam,nomCam, statusCam, nomStatus, index}){
+import ToggleSwitch from "../components/ToggleSwitch.js";
+import Live from "../components/Live";
+import React from 'react';
+import testModal from './ModalLive';
+import { useEffect, useState } from "react";
+
+function CadreCameras({ idCam, nomCam, statusCam, nomStatus, index }) {
     const [etat, setEtat] = useState("https://video.4x4vert.be/video")
     let toto = index
-    return ( 
-       <div>
-            {statusCam ===1 ?
-                <div  data-bs-toggle="tooltip" data-bs-placement="top" title="Cliquez pour visionner le live" className={`row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-3 offset-md-4 offset-1 offset-lg-3 cadreBouton ${nomStatus}`} data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
-                    <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-camera-video"></i></div>
-                    <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
-                    <ToggleSwitch nom={nomCam} setEtat={setEtat} status={statusCam} id = {idCam}/>
-                    <Live idName={nomStatus} etat={etat} index={toto} nomCam={nomCam}/>
+    return (
+        <div>
+            {statusCam === 1 ?
+                <div className='container'>
+                <div className={"p-1 bg-light row align-items-start shadow-sm col-11 col-sm-9 col-md-3 col-lg-8 offset-sm-2 offset-md-1 offset-1 offset-lg-3 cadreBouton"}>
+                    <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`} className="row">
+                        <div className="col-1 col-md-1 col-sm-2">
+                            <i class="bi bi-camera-video"></i>
+                            <div className="col-3 col-sm-2 col-md-1 offset">{nomCam}</div>
+                        
+                        </div>
+                        
+
+                    </div>
+                    <ToggleSwitch nom={nomCam} setEtat={setEtat} status={statusCam} id={idCam} />
+                    <Live idName={nomStatus} etat={etat} index={toto} nomCam={nomCam} />
+
+
                 </div> 
-            : <div  className={`row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-3 offset-md-4 offset-1 offset-lg-3 cadreBouton ${nomStatus}`}>  
-                <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-camera-video-off"></i></div>
-                <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
-                <ToggleSwitch nom={nomCam} status={statusCam} id = {idCam}/>
-                </div>
-            }    
-        </div> 
+                </div>:
+
+                statusCam === 2 ?
+                    <div data-bs-toggle="tooltip" data-bs-placement="top" title="Cette caméra est éteinte, allumez la pour visionner le live" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
+                        <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
+                            <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-camera-video-off"></i></div>
+                            <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
+
+                        </div>
+                        <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
+                    </div>
+
+                    :
+
+                    <div data-bs-toggle="tooltip" data-bs-placement="top" title="Il semblerait que cette caméra soit désactivée" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
+                        <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
+                            <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-exclamation-triangle"></i></div>
+                            <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
+
+                        </div>
+                        <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
+                    </div>
+
+            }
+        </div>
     )
- }
- export default CadreCameras; 
+}
+export default CadreCameras;
