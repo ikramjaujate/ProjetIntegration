ef0e20fc4a91fa35b5d4fcf06bf60b6fda7ffeec
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-12-19 17:13:09 +0100

    modale

--- frontend/src/components/OngletCamera.js
+++ frontend/src/components/OngletCamera.js
@@ -10,45 +10,48 @@ function CadreCameras({ idCam, nomCam, statusCam, nomStatus, index }) {
     const [etat, setEtat] = useState("https://video.4x4vert.be/video")
     let toto = index
     return (
-        <div>
+        <div className='cadreCamera'>
+
             {statusCam === 1 ?
-                <div className='container'>
-                <div className={"p-1 bg-light row align-items-start shadow-sm col-11 col-sm-9 col-md-3 col-lg-8 offset-sm-2 offset-md-1 offset-1 offset-lg-3 cadreBouton"}>
-                    <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`} className="row">
-                        <div className="col-1 col-md-1 col-sm-2">
-                            <i class="bi bi-camera-video"></i>
-                            <div className="col-3 col-sm-2 col-md-1 offset">{nomCam}</div>
-                        
+                
+                    <div className={"p-1 bg-light align-items-start shadow-sm col-11 col-sm-9 col-md-3 col-lg-8 offset-sm-2 offset-md-1 offset-1 offset-lg-3 cadreBouton"}>
+                        <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`} className="row">
+                            <div className="col-1 col-md-1 col-sm-2">
+                                <i className="bi bi-camera-video"></i>
+                                <div className="col-1 col-sm-2 col-md-1 offset">{nomCam}</div>
+
+                            </div>
+
+
                         </div>
-                        
+                        <ToggleSwitch nom={nomCam} setEtat={setEtat} status={statusCam} id={idCam} />
+                        <Live idName={nomStatus} etat={etat} index={toto} nomCam={nomCam} />
+
 
                     </div>
-                    <ToggleSwitch nom={nomCam} setEtat={setEtat} status={statusCam} id={idCam} />
-                    <Live idName={nomStatus} etat={etat} index={toto} nomCam={nomCam} />
-
-
-                </div> 
-                </div>:
+               :
 
                 statusCam === 2 ?
-                    <div data-bs-toggle="tooltip" data-bs-placement="top" title="Cette caméra est éteinte, allumez la pour visionner le live" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
-                        <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
-                            <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-camera-video-off"></i></div>
-                            <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
-
+                    <div className="container">
+                        <div data-bs-toggle="tooltip" data-bs-placement="top" title="Cette caméra est éteinte, allumez la pour visionner le live" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
+                            <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
+                                <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-camera-video-off"></i></div>
+                                <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
+                            </div>
+                            <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
                         </div>
-                        <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
                     </div>
 
                     :
+                    <div className="container">
+                        <div data-bs-toggle="tooltip" data-bs-placement="top" title="Il semblerait que cette caméra soit désactivée" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
+                            <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
+                                <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-exclamation-triangle"></i></div>
+                                <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
 
-                    <div data-bs-toggle="tooltip" data-bs-placement="top" title="Il semblerait que cette caméra soit désactivée" className={"p-1 bg-light shadow-sm row col-11 col-sm-9 col-md-7 col-lg-8 offset-sm-2 offset-md-3 offset-1 offset-lg-3 cadreBouton"}>
-                        <div type="button" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
-                            <div className="col-1 col-md-1 col-sm-2"><i class="bi bi-exclamation-triangle"></i></div>
-                            <div className="col-3 col-sm-2 col-md-2 offset">{nomCam}</div>
-
+                            </div>
+                            <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
                         </div>
-                        <ToggleSwitch nom={nomCam} status={statusCam} id={idCam} />
                     </div>
 
             }
--- frontend/src/components/ToggleSwitch.js
+++ frontend/src/components/ToggleSwitch.js
@@ -1,54 +1,54 @@
 import React from "react";
 import "../css/ToggleSwitch.css";
 import { useEffect, useState } from "react";
-  
-const ToggleSwitch = ({ nom,status,id , setEtat }) => {
-  //const [etat, setEtat] = useState("http://0.0.0.0:6060/video")
-  function Allumer() {
-    console.log("up")
-    fetch("http://0.0.0.0:6060/up", {
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        mode: 'no-cors'
-    }).then(res => res.json).then(data => {
-        setEtat("http://0.0.0.0:6060/video")
-    })
-}
-function Eteindre() {
-  console.log("éteindre")
-  fetch("http://0.0.0.0:6060/shutdown", {
-      headers: {
-          'Access-Control-Allow-Origin': '*'
-      },
-      mode: 'no-cors'
-  }).then(res => res.json).then(data => {
-      setEtat('camera-disconnected.png')
-  })
-}
-  function changeStatusCam(identifiant) {
-    let statut = document.getElementById(identifiant).checked
-    if (statut) {
-        Allumer();
+
+const ToggleSwitch = ({ nom, status, id, setEtat }) => {
+    //const [etat, setEtat] = useState("http://0.0.0.0:6060/video")
+    function Allumer() {
+        console.log("up")
+        fetch("http://0.0.0.0:6060/up", {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'no-cors'
+        }).then(res => res.json).then(data => {
+            setEtat("http://0.0.0.0:6060/video")
+        })
     }
-    else {
-        Eteindre();
+    function Eteindre() {
+        console.log("éteindre")
+        fetch("http://0.0.0.0:6060/shutdown", {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'no-cors'
+        }).then(res => res.json).then(data => {
+            setEtat('camera-disconnected.png')
+        })
     }
-}
-    
-  return (
-    <div className='col-2 col-md-2 col-sm-2 offset-4 offset-md-5 offset-sm-4 offset-lg-7 switchcam'>
-                        <div class="form-check form-switch">
-                            {status === 1 ? <input class="form-check-input" type="checkbox" role="switch" id={id} defaultChecked onClick={() => changeStatusCam(id)} /> :
-                                status === 2 ? <input class="form-check-input" type="checkbox" role="switch" id={id} onClick={() => changeStatusCam(id)} /> :
-                                    <input class="form-check-input" type="checkbox" role="switch" id={id} disabled />}
-                        </div>
-                    
-    </div>
+    function changeStatusCam(identifiant) {
+        let statut = document.getElementById(identifiant).checked
+        if (statut) {
+            Allumer();
+        }
+        else {
+            Eteindre();
+        }
+    }
+
+    return (
+        <div className='perm col-2 col-md-2 col-sm-2 offset-4 offset-md-5 offset-sm-0 offset-lg-7 switchcam'>
+            <div className="form-check form-switch">
+                {status === 1 ? <input className="form-check-input" type="checkbox" role="switch" id={id} defaultChecked onClick={() => changeStatusCam(id)} /> :
+                    status === 2 ? <input className="form-check-input" type="checkbox" role="switch" id={id} onClick={() => changeStatusCam(id)} /> :
+                        <input class="form-check-input" type="checkbox" role="switch" id={id} disabled />}
+            </div>
+
+        </div>
 
 
 
-  );
+    );
 };
-  
+
 export default ToggleSwitch;
