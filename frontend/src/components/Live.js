5eccab6e23ed9163ba1ee6118fd923f17325af70
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-12-22 16:51:34 +0100

    Ajout screenshot

--- frontend/src/components/Live.js
+++ frontend/src/components/Live.js
@@ -1,16 +1,20 @@
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
-import'bootstrap/dist/css/bootstrap.min.css';
-import'bootstrap/dist/js/bootstrap.min.js';
-import'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Popover, Toast, Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js' ;
+import { Popover, Toast, Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect, useState } from "react";
+import  ikram from './ikram2.jpg';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
-function Live({etat,index,nomCam}){
+
+function Live({ etat, index, nomCam }) {
   const [screenshoot, setScreenshoot] = useState(null);
   const optionsToast = {
     autoClose: 5000,
@@ -20,71 +24,96 @@ function Live({etat,index,nomCam}){
     pauseOnHover: true,
     draggable: true,
     theme: "colored"
-};
+  };
 
-  function Capture() {
+  async function Capture() {
     //event.preventDefault()
-    console.log("capture")
-    fetch("http://192.168.1.3:6060/photo", {
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        mode: 'no-cors'
+    let date = new Date();
+    let nomFichier = '../public/image-client/frame_' + date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay() + '_' + date.getHours() + '_' + date.getMinutes() + '_' + date.getSeconds() + '.jpeg'
+
+    html2canvas(document.getElementById(`body-${index}`)).then(function (canvas) {
+      // document.body.appendChild(canvas);
+      let picture = canvas.toDataURL('image/jpeg')
+      
+      saveAs(picture, nomFichier)
     })
-        .then(res => res.json)
-        .then(data => {
-            fetch("/api/pictureScreenshoot", {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then(res => res.json())
-                .then(data => {
-                    setScreenshoot(data.picture)
-                    const ScreenshotToast = () => (
-                        <div >
-                            <img class="layout-screenshot" data-bs-toggle="modal" data-bs-target="#openscreenshotmodal" src={`image-client/${data.picture}`} alt="video surveillance" width="100" height="auto" />
-                        </div>
-                    )
-                    toast.info(<ScreenshotToast />, optionsToast);
-                })
-        })
+    console.log("capture")
+    // await fetch("/api/photos/decrypt", {
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*'
+    //   },
+    //   mode: 'no-cors'
+    // })
+    // fetch("http://0.0.0.0:6060/photo", {
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*'
+    //   },
+    //   mode: 'no-cors'
+    // })
+    //   .then(res => res.json)
+    //   .then(data => {
+    //     fetch("/api/pictureScreenshoot", {
+    //       method: 'GET',
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       }
+    //     }).then(res => res.json())
+    //       .then(async (data) => {
+    //         setScreenshoot(data.picture)
 
-}
+    //         const ScreenshotToast = () => (
+    //           <div >
+    //             <img class="layout-screenshot" data-bs-toggle="modal" data-bs-target="#openscreenshotmodal" src={`imgClient/${data.picture}`} alt="video surveillance" width="100" height="auto" />
+    //           </div>
+    //         )
+    //         toast.info(<ScreenshotToast />, optionsToast);
+    //         fetch("/api/photos/encrypt", {
+    //           headers: {
+    //             'Access-Control-Allow-Origin': '*'
+    //           },
+    //           mode: 'no-cors'
+    //         })
+    //       })
+
+      // })
+
+  }
 
 
-return(
-  <>
-    <div class="modal fade" id={`modal-${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 style={{'color':'grey'}} class="modal-title" id="staticBackdropLabel">{nomCam}</h5>
-            <button type="button" class=" btn-secondary btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <img className="img-thumbnail" alt='visu_live' src='https://video.4x4vert.be/video' width="440" height="300" title="Foscam FI8905W" />
-          </div>
-          <div class="modal-footer justify-content-center">
-            
-            <button type="button" onClick={Capture} className="btn btn-lg m-2 save-button"><i className="bi bi-camera"></i></button>
+  return (
+    <>
+      <div class="modal fade" id={`modal-${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 style={{ 'color': 'grey' }} class="modal-title" id="staticBackdropLabel">{nomCam}</h5>
+              <button type="button" class=" btn-secondary btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <img id={`body-${index}`} className="img-thumbnail" alt='visu_live' src={ikram} width="440" height="300" title="Foscam FI8905W" />
+              {/* <img className="img-thumbnail" alt='visu_live' src='https://video.4x4vert.be/video' width="440" height="300" title="Foscam FI8905W" /> */}
+            </div>
+            <div class="modal-footer justify-content-center">
+
+              <button type="button" onClick={Capture} className="btn btn-lg m-2 save-button"><i className="bi bi-camera"></i></button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <ToastContainer style={{ fontSize: "0.6rem" }} />
-            <div id="openscreenshotmodal" className="modal fade" data-bs-keyboard="false" tabindex="-1" aria-labelledby="gradeModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-                    <div className="modal-content">
+      {/* <ToastContainer style={{ fontSize: "0.6rem" }} />
+      <div id="openscreenshotmodal" className="modal fade" data-bs-keyboard="false" tabindex="-1" aria-labelledby="gradeModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
 
-                        <div className="modal-body">
-                            <img class="modal-screenshot" src={`image-client/${screenshoot}`} alt="video surveillance" width="100%" height="100%" />
-                        </div>
-
-                    </div>
-                </div>
+            <div className="modal-body" >
+              <img class="modal-screenshot" src={`imgClient/${screenshoot}`} alt={'Screenshot'} />
             </div>
+
+          </div>
+        </div>
+      </div> */}
     </>
-    
-)}
-export default Live; 
+
+  )
+}
+export default Live;
